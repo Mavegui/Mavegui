fb31a348ffa529e8143488c52f2a183f363b4d36
Guilherme F. <40839511+Mavegui@users.noreply.github.com>
2026-07-09 13:15:39 -0300

    Create message list in update_msg.py

---
+++ update_msg.py
@@ -0,0 +1,25 @@
+import random
+import re
+
+# Seu banco de dados de frases personalizado
+msg = [
+    "Ainda vou add mensagens aqui, lol!"
+    "Pensando ainda..."
+    "Trabalhando..."
+]
+
+def update_readme():
+    frase_do_dia = random.choice(msg)
+    
+    with open("README.md", "r", encoding="utf-8") as file:
+        conteudo = file.read()
+
+    # regex para mostrar texto.
+    padrao = r"(<!-- VARIABLE:START -->)(.*?)(<!-- VARIABLE:END -->)"
+    novo_conteudo = re.sub(padrao, f"\\1\n\n> {frase_do_dia}\n\n\\3", conteudo, flags=re.DOTALL)
+
+    with open("README.md", "w", encoding="utf-8") as file:
+        file.write(novo_conteudo)
+
+if __name__ == "__main__":
+    update_readme()
